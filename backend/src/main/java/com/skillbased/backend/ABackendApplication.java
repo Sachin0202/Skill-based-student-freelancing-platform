1ac9a9f790d03b62694e6549ac2fb4a178d0bd26
Sachin <143882687+Sachin0202@users.noreply.github.com>
2026-06-09 12:30:17 +0530

    dockerf

--- backend/src/main/java/com/skillbased/backend/ABackendApplication.java
+++ backend/src/main/java/com/skillbased/backend/ABackendApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ABackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ABackendApplication.class, args);
+
+        SpringApplication.run(ABackendApplication.class, args);
 	}
 
 }
